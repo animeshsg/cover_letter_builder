c5334b785076017b13ac8fd161083d0e30653722
animeshsg <asg.animesh@gmail.com>
2022-12-16 21:01:22 -0500

    [ADD] Html templates

--- builder.py
+++ builder.py
@@ -1,24 +1,48 @@
 import jinja2
 import os
 import sys
-
-TEMPLATES_FOLDER="./templates"
-
-
 import argparse
 from pathlib import Path
+from jinja2 import Environment, FileSystemLoader, TemplateNotFound
+import datetime
 
-parser = argparse.ArgumentParser()
 
-parser.add_argument("path")
+TEMPLATES_FOLDER="./templates/"
+OUTPUT_FOLDER="./cover_letters"
+DEFAULT_TEMPLATE="template1.html.j2"
+DEFAULT_NAME="Animesh Sengupta"
 
-args = parser.parse_args()
+def create(role,org,template=DEFAULT_TEMPLATE,name=DEFAULT_NAME):
+    date = f"{datetime.datetime.now():%m/%d/%y}"
+    try:
+        environment = Environment(loader=FileSystemLoader(TEMPLATES_FOLDER))
+        template = environment.get_template(DEFAULT_TEMPLATE)
+        rendered_ouput=template.render(
+            role=role,
+            org=org,
+            name=name,
+            date=date)
+        print (rendered_ouput)
+    except TemplateNotFound as e:
+        print("Error 404: Template File/Folder Not found",e)
 
-target_dir = Path(args.path)
 
-if not target_dir.exists():
-    print("The target directory doesn't exist")
-    raise SystemExit(1)
 
-for entry in target_dir.iterdir():
-    print(entry.name)
+
+create("Data Engineer","Google")
+
+
+# parser = argparse.ArgumentParser()
+
+# parser.add_argument("path")
+
+# args = parser.parse_args()
+
+# target_dir = Path(args.path)
+
+# if not target_dir.exists():
+#     print("The target directory doesn't exist")
+#     raise SystemExit(1)
+
+# for entry in target_dir.iterdir():
+#     print(entry.name)
